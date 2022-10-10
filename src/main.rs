5e67fe3cbde5ffccbd292e8a95aa6d6161416054
randiekas <randiekas@gmail.com>
2022-10-10 15:15:27 +0700

    update response

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, Responder, Result};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct MyObj {
+    status: bool,
+    message: String,
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index() -> Result<impl Responder> {
+    let obj = MyObj {
+        status: true,
+        message: "Hello world".to_string(),
+    };
+    Ok(web::Json(obj))
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    use actix_web::{App, HttpServer};
+
+    HttpServer::new(|| App::new().service(index))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
